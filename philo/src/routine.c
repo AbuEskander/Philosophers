5e74d559d98fadaf18a2fa73a9f36aeed380ce4a
Proton <basselawni1@gmail.com>
2024-12-26 12:30:10 +0300

    Separated The lock functions so I can handle mutex interfering with eachother

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,11 +6,38 @@
 /*   By: abueskander <abueskander@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 12:08:54 by abueskander       #+#    #+#             */
-/*   Updated: 2024/12/25 12:26:23 by abueskander      ###   ########.fr       */
+/*   Updated: 2024/12/25 16:42:36 by abueskander      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
+
+static void	lock_forks(t_philosofo *philoso)
+{
+	if (philoso->id % 2 == 0)
+	{
+		pthread_mutex_lock(philoso->rightf);
+		pthread_mutex_lock(philoso->leftf);
+	}
+	else
+	{
+		pthread_mutex_lock(philoso->leftf);
+		pthread_mutex_lock(philoso->rightf);
+	}
+}
+static void	unlock_forks(t_philosofo *philoso)
+{
+	if (philoso->id % 2 == 0)
+	{
+		pthread_mutex_unlock(philoso->leftf);
+		pthread_mutex_unlock(philoso->rightf);
+	}
+	else
+	{
+		pthread_mutex_unlock(philoso->rightf);
+		pthread_mutex_unlock(philoso->leftf);
+	}
+}
 
 void	*routine(void *args)
 {
@@ -22,18 +49,7 @@ void	*routine(void *args)
 		printf(" %llu ms  %d is sleeping \n", get_time_fixed()
 				- philoso->last_meal, philoso->id);
 		usleep(philoso->tts * 1000);
-		// 2 //
-		if (philoso->id % 2 == 0)
-		{
-			pthread_mutex_lock(philoso->rightf);
-			pthread_mutex_lock(philoso->leftf);
-		}
-		else
-		{
-			pthread_mutex_lock(philoso->leftf);
-			pthread_mutex_lock(philoso->rightf);
-		}
-		
+		lock_forks(philoso);
 		printf(" %llu ms  %d has taken a fork \n", get_time_fixed()
 				- philoso->last_meal, philoso->id);
 		printf(" %llu ms  %d has taken a fork \n", get_time_fixed()
@@ -42,16 +58,8 @@ void	*routine(void *args)
 				- philoso->last_meal, philoso->id);
 		usleep(philoso->tte * 1000);
 		philoso->nuofm--;
-		if (philoso->id % 2 == 0)
-		{
-			pthread_mutex_unlock(philoso->leftf);
-			pthread_mutex_unlock(philoso->rightf);
-		}
-		else
-		{
-			pthread_mutex_unlock(philoso->rightf);
-			pthread_mutex_unlock(philoso->leftf);
-		}
+		unlock_forks(philoso);
+	
 	}
 	return (NULL);
 }
