19f9172803bbd40b3ec81b80cfb76877bbeb452b
Basil Ismail <bismail@c2r2s1.42amman.com>
2025-01-22 13:02:14 +0300

    Done

--- philo/src/threads.c
+++ philo/src/threads.c
@@ -6,7 +6,7 @@
 /*   By: bismail <bismail@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 11:28:44 by abueskander       #+#    #+#             */
-/*   Updated: 2025/01/22 12:59:38 by bismail          ###   ########.fr       */
+/*   Updated: 2025/01/22 13:02:07 by bismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,10 @@ int	init_forks(t_table *table)
 	table->print = malloc(sizeof(pthread_mutex_t));
 	if (!table->print)
 		return (EXIT_FAILURE);
-	pthread_mutex_init(table->print, NULL);
-	pthread_mutex_init(table->dead, NULL);
+	if (pthread_mutex_init(table->print, NULL) == -1)
+		return (EXIT_FAILURE);
+	if (pthread_mutex_init(table->dead, NULL) == -1)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
 
